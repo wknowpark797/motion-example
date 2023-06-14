d90159891cc69843286c8bab981b178ff21670ed
wknowpark797 <wknowpark797@gmail.com>
2023-06-14 12:40:45 +0900

    scroll motion 브라우저 리사이즈 이슈 처리

--- js/scrollMotion.js
+++ js/scrollMotion.js
@@ -10,11 +10,13 @@
 */
 
 const sections = document.querySelectorAll('section');
-const btns = document.querySelectorAll('ul li');
+const list = document.querySelector('ul');
+const btns = list.querySelectorAll('li');
 const speed = 1000;
 let preventEvent = true; // 모션 실행 중 여부
 
 window.addEventListener('scroll', activation);
+window.addEventListener('resize', modifyPos);
 
 btns.forEach((btn, idx) => {
 	btn.addEventListener('click', () => preventEvent && moveScroll(idx));
@@ -47,4 +49,13 @@ function moveScroll(idx) {
 		duration: speed,
 		callback: () => (preventEvent = true),
 	});
+}
+
+// 브라우저 resize 시 현재 스크롤 위치값 갱신 함수
+function modifyPos() {
+	const active = list.querySelector('li.on');
+	const activeIdx = Array.from(btns).indexOf(active);
+
+	// scroll(가로 위치값, 세로 위치값) - 해당 위치로 바로 이동
+	window.scroll(0, sections[activeIdx].offsetTop);
 }
